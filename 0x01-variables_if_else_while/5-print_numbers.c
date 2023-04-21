851b984fffa0e3f288b31cc70eab5d2e0655edc5
osmmacfides <okonkwosmmac@gmail.com>
2023-04-21 16:06:06 -0700

    self

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d \n", n);
+	}
+	return (0);
+}
