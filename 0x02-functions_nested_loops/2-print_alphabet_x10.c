22c96a84b49780055c4463c0baa25da437dd36e5
osmmacfides <okonkwosmmac@gmail.com>
2023-04-25 19:30:53 -0700

    all

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,25 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
 
+	i = 0;
+	while (i <= 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		i++;
+	}
+
+}
